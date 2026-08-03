c2a306d11766a0f1065d155dc1d79d21e9903d17
bhaskhar-159 <bhaskarma15191@gmail.com>
2026-08-03 23:54:59 +0530

    Recruiter Applicant Management (ATS)

--- applications/urls.py
+++ applications/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
 
     path("apply/<int:job_id>/", views.apply_job, name="apply_job"),
     path("my/", views.my_applications, name="my_applications"),
+    path("job/<int:job_id>/applicants/", views.applicant_list, name="applicant_list"),
+    path("<int:application_id>/", views.applicant_detail, name="applicant_detail"),
+    path("<int:application_id>/status/", views.update_application_status, name="update_application_status"),
 
 ]
--- applications/views.py
+++ applications/views.py
@@ -96,3 +96,94 @@ def my_applications(request):
             "applications": applications
         }
     )    
+    
+    
+@login_required(login_url="login")
+def applicant_list(request, job_id):
+
+    profile = request.user.profile
+
+    if profile.role != "RECRUITER":
+        return HttpResponseForbidden("Access Denied")
+
+    job = get_object_or_404(
+        Job,
+        id=job_id,
+        company__recruiter=profile.recruiter
+    )
+
+    applications = Application.objects.filter(
+        job=job
+    ).select_related(
+        "applicant",
+        "applicant__user_profile",
+        "applicant__user_profile__user"
+    )
+
+    return render(
+        request,
+        "applications/applicant_list.html",
+        {
+            "job": job,
+            "applications": applications
+        }
+    )    
+    
+    
+@login_required(login_url="login")
+def applicant_detail(request, application_id):
+
+    profile = request.user.profile
+
+    if profile.role != "RECRUITER":
+        return HttpResponseForbidden("Access Denied")
+
+    application = get_object_or_404(
+        Application,
+        id=application_id,
+        job__company__recruiter=profile.recruiter
+    )
+
+    return render(
+        request,
+        "applications/applicant_detail.html",
+        {
+            "application": application
+        }
+    )    
+    
+    
+    
+@login_required(login_url="login")
+def update_application_status(request, application_id):
+
+    profile = request.user.profile
+
+    if profile.role != "RECRUITER":
+        return HttpResponseForbidden("Access Denied")
+
+    application = get_object_or_404(
+        Application,
+        id=application_id,
+        job__company__recruiter=profile.recruiter
+    )
+
+    if request.method == "POST":
+
+        application.status = request.POST["status"]
+
+        application.save()
+
+        return redirect(
+            "applicant_detail",
+            application.id
+        )
+
+    return render(
+        request,
+        "applications/update_status.html",
+        {
+            "application": application,
+            "choices": Application.STATUS_CHOICES
+        }
+    )    
